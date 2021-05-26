db882335773e79f315deb8f61701cee2a3c4d5bd
donngchao <1559273720@qq.com>
2021-05-26 10:28:11 +0800

    update codes

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: donngchao
+@file: main.py
+@time: 2021/5/25 1:12 下午
+@desc: This file will help you compare two different files
+'''
+
+from flask import Flask, render_template, request
+import difflib
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def upload_file():
+    return render_template("input.html")
+
+
+@app.route('/compare', methods=['POST', 'GET'])
+def compare():
+    if request.method == 'POST':
+        file1 = request.form['file1']
+        file2 = request.form['file2']
+        text1_line = file1.splitlines()
+        text2_line = file2.splitlines()
+        d = difflib.HtmlDiff()
+        return d.make_file(text1_line, text2_line)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8081)
